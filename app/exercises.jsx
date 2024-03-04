2d8b9f8d4bef5c0df8f23fd86436db8561ed20f5
Kevin Rojas <Kred2259@gmail.com>
2024-03-03 20:17:19 -0500

    Worked on the start page and the home page which includes a carousel and categories of excercises

---
+++ app/exercises.jsx
@@ -0,0 +1,17 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+
+export default function Exercises() {
+    const router = useRouter();
+    const item = useLocalSearchParams();
+    console.log('got item: ', item);
+  return (
+    <View className="mt-20">
+      <Text>Exercises</Text>
+      <TouchableOpacity onPress={()=> router.back()}>
+        <Text>go back</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
---
+++ constants/Index.js
@@ -0,0 +1,50 @@
+export const sliderImages = [
+    require('../assets/images/slide1.png'),
+    require('../assets/images/slide2.png'),
+    require('../assets/images/slide3.png'),
+    require('../assets/images/slide4.png'),
+    require('../assets/images/slide5.png'),
+]
+
+export const bodyParts = [
+    {
+        name: 'back',
+        image: require('../assets/images/back.png'),
+    },
+    {
+        name: 'cardio',
+        image: require('../assets/images/cardio.png'),
+    },
+    {
+        name: 'lower arms',
+        image: require('../assets/images/lowerArms.png'),
+    },
+    {
+        name: 'lower legs',
+        image: require('../assets/images/lowerLegs.png'),
+    },
+    {
+        name: 'chest',
+        image: require('../assets/images/chest.png'),
+    },
+    {
+        name: 'neck',
+        image: require('../assets/images/neck.png'),
+    },
+    {
+        name: 'shoulders',
+        image: require('../assets/images/shoulders.png'),
+    },
+    {
+        name: 'upper arms',
+        image: require('../assets/images/upperArms.png'),
+    },
+    {
+        name: 'upper legs',
+        image: require('../assets/images/upperLegs.png'),
+    },
+    {
+        name: 'waist',
+        image: require('../assets/images/waist.png'),
+    }
+]
