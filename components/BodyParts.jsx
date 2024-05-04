d39e8d9c5b592239baafafdd46d49be71a4f6842
methiladeb <methiladeb@gmail.com>
2024-05-04 19:16:39 -0400

    components update

--- components/BodyParts.jsx
+++ components/BodyParts.jsx
@@ -1,62 +1,61 @@
-import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { bodyParts } from '../constants';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import Animated, { FadeInDown } from 'react-native-reanimated';
 
-export default function BodyParts() {
-    const router = useRouter();
-  return (
-    <View className="mx-4">
-      <Text style={{fontSize: hp(3)}} className="font-semibold text-neutral-700">
-        Exercises
-      </Text>
-
-      <FlatList
-        data={bodyParts}
-        numColumns={2}
-        keyExtractor={item=> item.name}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{paddingBottom: 50, paddingTop: 20}}
-        columnWrapperStyle={{
-            justifyContent: 'space-between'
-        }}
-        renderItem={({item, index})=> <BodyPartCard router={router} index={index} item={item} />}
-      />
-    </View>
-  )
-}
-
-const BodyPartCard = ({item, router, index})=>{
+function BodyPartsList() {
+    const navigation = useRouter();
     return (
-        <Animated.View entering={FadeInDown.duration(400).delay(index*200).springify()}>
-            <TouchableOpacity
-                onPress={()=> router.push({pathname: '/exercises', params: item})}
-                style={{width: wp(44), height: wp(52)}}
-                className="flex justify-end p-4 mb-4">
-                    <Image 
-                        source={item.image}
-                        resizeMode='cover'
-                        style={{width: wp(44), height: wp(52)}}
-                        className="rounded-[35px] absolute"
-                    />
-                    <LinearGradient
-                        colors={['transparent', 'rgba(0,0,0,0.9)']}
-                        style={{width: wp(44), height: hp(15)}}
-                        start={{x: 0.5, y: 0}}
-                        end={{x: 0.5, y: 1}}
-                        className="absolute bottom-0 rounded-b-[35px]"
-                    />
+        <View style={{ marginHorizontal: 16 }}>
+            <Text style={{ fontSize: hp(3), fontWeight: '600', color: '#4B5563' }}>
+                Exercises
+            </Text>
 
-                    <Text
-                        style={{fontSize: hp(2.3)}}
-                        className="text-white font-semibold text-center tracking-wide"
-                    >
-                        {item?.name}
-                    </Text>
-            </TouchableOpacity>
-        </Animated.View>
+            <FlatList
+                data={bodyParts}
+                numColumns={2}
+                keyExtractor={bodyPart => bodyPart.name}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 50, paddingTop: 20 }}
+                columnWrapperStyle={{ justifyContent: 'space-between' }}
+                renderItem={({ item, index }) => <ExerciseCard navigation={navigation} index={index} bodyPart={item} />}
+            />
+        </View>
     )
 }
+
+const ExerciseCard = ({ bodyPart, navigation, index }) => {
+    return (
+        <Animated.View entering={FadeInDown.duration(400).delay(index * 200).springify()}>
+            <TouchableOpacity
+                onPress={() => navigation.push({ pathname: '/exercises', params: bodyPart })}
+                style={{ width: wp(44), height: wp(52), marginBottom: 16 }}
+                className="items-end px-4">
+                <Image
+                    source={bodyPart.image}
+                    resizeMode='cover'
+                    style={{ width: wp(44), height: wp(52), borderRadius: 35, position: 'absolute' }}
+                />
+                <LinearGradient
+                    colors={['transparent', 'rgba(0,0,0,0.9)']}
+                    style={{ width: wp(44), height: hp(15), borderRadius: 35 }}
+                    start={{ x: 0.5, y: 0 }}
+                    end={{ x: 0.5, y: 1 }}
+                    className="absolute bottom-0"
+                />
+
+                <Text
+                    style={{ fontSize: hp(2.3), color: '#FFFFFF', fontWeight: '600', textAlign: 'center', letterSpacing: 1 }}
+                    className="w-full"
+                >
+                    {bodyPart?.name}
+                </Text>
+            </TouchableOpacity>
+        </Animated.View>
+    );
+}
+
+export default BodyPartsList;
--- components/ExerciseList.jsx
+++ components/ExerciseList.jsx
@@ -1,51 +1,49 @@
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useRouter } from 'expo-router'
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { useRouter } from 'expo-router';
 import { Image } from 'expo-image';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Animated, { FadeInDown } from 'react-native-reanimated';
 
-export default function ExerciseList({data}) {
+function ExerciseList({ data }) {
     const router = useRouter();
-  return (
-    <View>
-      <FlatList
-        data={data}
-        numColumns={2}
-        keyExtractor={item=> item.name}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{paddingBottom: 60, paddingTop: 20}}
-        columnWrapperStyle={{
-            justifyContent: 'space-between'
-        }}
-        renderItem={({item, index})=> <ExerciseCard router={router} index={index} item={item} />}
-      />
-    </View>
-  )
+    return (
+        <View style={{ flex: 1 }}>
+            <FlatList
+                data={data}
+                numColumns={2}
+                keyExtractor={exercise => exercise.name}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 60, paddingTop: 20 }}
+                columnWrapperStyle={{ justifyContent: 'space-between' }}
+                renderItem={({ item, index }) => <WorkoutDetail router={router} index={index} item={item} />}
+            />
+        </View>
+    );
 }
 
-const ExerciseCard = ({item, router, index})=> {
+const WorkoutDetail = ({ item, router, index }) => {
     return (
-        <Animated.View entering={FadeInDown.duration(400).delay(index*200).springify()}>
-            <TouchableOpacity onPress={()=> router.push({pathname: '/exerciseDetails', params: item})} className="flex py-3 space-y-2">
-                <View  className="bg-white shadow rounded-[25px]">
+        <Animated.View entering={FadeInDown.duration(400).delay(index * 200).springify()}>
+            <TouchableOpacity
+                onPress={() => router.push({ pathname: '/exerciseDetails', params: item })}
+                style={{ padding: 12, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between' }}>
+                <View style={{ backgroundColor: 'white', elevation: 4, borderRadius: 25, width: wp(44), height: wp(52) }}>
                     <Image
-                        source={{uri: item.gifUrl}}
-                        contentFit='cover'
-                        style={{width: wp(44), height: wp(52)}}
-                        className="rounded-[25px]"
+                        source={{ uri: item.gifUrl }}
+                        style={{ width: '100%', height: '100%', borderRadius: 25 }}
+                        contentFit="cover"
                     />
                 </View>
 
-                <Text  
-                    style={{fontSize: hp(1.7)}}
-                    className="text-neutral-700 font-semibold ml-1  tracking-wide"
+                <Text
+                    style={{ fontSize: hp(1.7), color: '#4A5568', fontWeight: 'bold', marginTop: 8 }}
                 >
-                    {
-                        item?.name?.length>20? item.name.slice(0,20)+'...': item.name
-                    }
+                    {item?.name.length > 20 ? `${item.name.substring(0, 20)}...` : item.name}
                 </Text>
             </TouchableOpacity>
         </Animated.View>
-    )
+    );
 }
+
+export default ExerciseList;
